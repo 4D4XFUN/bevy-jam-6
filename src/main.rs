ab8cbafe863c37fcaccde27c4e4cfe830250f299
Sam Farmer <sam@rocketscienceamusements.com>
2025-06-01 01:51:35 -0700

    More setup (#20)
    * made web build auto-open using rustrover ide
    
    * enabled vsync, added more detail to Environment.gltf, cleaned up some dependencies.
    
    * fmt

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ impl Plugin for AppPlugin {
                 .set(WindowPlugin {
                     primary_window: Window {
                         title: "Boomerang Showdown".to_string(),
-                        present_mode: PresentMode::AutoNoVsync, // turn off vsync to prevent input lag
+                        present_mode: PresentMode::AutoVsync,
                         fit_canvas_to_parent: true,
                         ..default()
                     }
