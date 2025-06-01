5e51bba7bc2c5bf87fbd450018a31e7df8b1be8c
Sam Farmer <sam@rocketscienceamusements.com>
2025-05-31 23:06:08 -0700

    enabled vsync, added more detail to Environment.gltf, cleaned up some dependencies.

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,25 +1,43 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
-use bevy::{
-    dev_tools::states::log_transitions, input::common_conditions::input_just_pressed, prelude::*,
-    ui::UiDebugOptions,
-};
-
+use bevy::dev_tools::states::log_transitions;
+use bevy::prelude::*;
+use iyes_perf_ui::entries::{PerfUiFramerateEntries, PerfUiWindowEntries};
+use iyes_perf_ui::PerfUiPlugin;
+use iyes_perf_ui::prelude::{PerfUiPosition, PerfUiRoot};
 use crate::screens::Screen;
 
 pub(super) fn plugin(app: &mut App) {
+    app.add_plugins((
+        PerfUiPlugin,
+        bevy::diagnostic::FrameTimeDiagnosticsPlugin::default(),
+        bevy::diagnostic::EntityCountDiagnosticsPlugin,
+        // https://github.com/IyesGames/iyes_perf_ui/issues/30
+        // bevy::diagnostic::SystemInformationDiagnosticsPlugin,
+        bevy::render::diagnostic::RenderDiagnosticsPlugin,
+    ));
+
     // Log `Screen` state transitions.
     app.add_systems(Update, log_transitions::<Screen>);
 
-    // Toggle the debug overlay for UI.
-    app.add_systems(
-        Update,
-        toggle_debug_ui.run_if(input_just_pressed(TOGGLE_KEY)),
-    );
+    app.add_systems(Startup, setup_perf_ui);
 }
 
-const TOGGLE_KEY: KeyCode = KeyCode::Backquote;
+#[derive(Component)]
+pub struct PerfUiMarker;
 
-fn toggle_debug_ui(mut options: ResMut<UiDebugOptions>) {
-    options.toggle();
+fn setup_perf_ui(mut commands: Commands) {
+    commands.spawn((
+        PerfUiMarker,
+        PerfUiRoot {
+            position: PerfUiPosition::TopRight,
+            ..default()
+        },
+        // Contains everything related to FPS and frame time
+        PerfUiFramerateEntries::default(),
+        // Contains everything related to the window and cursor
+        PerfUiWindowEntries::default(),
+        // Contains everything related to system diagnostics (CPU, RAM)
+        // PerfUiSystemEntries::default(),
+    ));
 }
