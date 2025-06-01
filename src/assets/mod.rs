ddae27462ad15314032686a853e38db3542c943b
tigerplush <64077786+tigerplush@users.noreply.github.com>
2025-06-01 13:53:44 +0200

    added some flair to the ui (#32)
    * added some fonts, different ui buttons and sounds to title
    
    * changed credits and settings screen
    
    * fmt & fix clippy

---
+++ src/assets/mod.rs
@@ -0,0 +1,5 @@
+mod font_assets;
+mod panel_assets;
+
+pub use font_assets::FontAssets;
+pub use panel_assets::PanelAssets;
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
 
 mod asset_tracking;
+mod assets;
 mod audio;
 mod demo;
 #[cfg(feature = "dev")]
@@ -11,6 +12,7 @@ mod dev_tools;
 mod screens;
 mod theme;
 
+use bevy::image::ImageSamplerDescriptor;
 use bevy::window::PresentMode;
 use bevy::{asset::AssetMetaCheck, prelude::*};
 use bevy_inspector_egui::bevy_egui::EguiPlugin;
@@ -58,6 +60,9 @@ impl Plugin for AppPlugin {
                     }
                     .into(),
                     ..default()
+                })
+                .set(ImagePlugin {
+                    default_sampler: ImageSamplerDescriptor::nearest(),
                 }),
             SkeinPlugin::default(),
             EguiPlugin {
