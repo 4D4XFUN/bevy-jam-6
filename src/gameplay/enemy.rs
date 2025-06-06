7152e3580da604348f408716a0140b731e200d75
jacudibu <jacudibu@gmail.com>
2025-06-06 21:42:53 +0200

    ...okay.

--- src/gameplay/enemy.rs
+++ src/gameplay/enemy.rs
@@ -5,8 +5,8 @@ use crate::gameplay::{boomerang::BoomerangHittable, health_and_damage::Health};
 use crate::physics_layers::GameLayer;
 use crate::screens::Screen;
 use avian3d::prelude::{
-    Collider, CollisionEventsEnabled, CollisionLayers, LinearVelocity, Physics, PhysicsLayer, RigidBody,
-    SpatialQuery, SpatialQueryFilter,
+    Collider, CollisionEventsEnabled, CollisionLayers, LinearVelocity, Physics, PhysicsLayer,
+    RigidBody, SpatialQuery, SpatialQueryFilter,
 };
 use bevy::color;
 use bevy::ecs::entity::EntityHashSet;
